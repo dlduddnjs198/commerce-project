0e8e6ceeaac0722d52c2639b2dd17fed745c7b7e
Young1 <dlduddnjs198@naver.com>
2023-07-02 09:34:09 +0900

    feat : [001-SIGNUP] DB ERD 테이블 Entity 구현

---
+++ src/main/java/com/zerobase/commerce/CartItem.java
@@ -0,0 +1,27 @@
+package com.zerobase.commerce;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "productId", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Long quantity;
+}
