f3766bc1d260da57e789f6e830d3bc6d0b450861
Young1 <dlduddnjs198@naver.com>
2023-07-04 06:44:40 +0900

    refactor : [User] 회원가입 validation 추가 및 리턴값 변경

--- src/main/java/com/zerobase/commerce/user/dto/form/SignUpForm.java
+++ src/main/java/com/zerobase/commerce/user/dto/form/SignUpForm.java
@@ -1,7 +1,10 @@
 package com.zerobase.commerce.user.dto.form;
 
 import com.zerobase.commerce.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -9,9 +12,19 @@ import lombok.*;
 @ToString
 @AllArgsConstructor
 public class SignUpForm {
+    @NotBlank(message = "id는 필수 입력 항목입니다.")
+    @Length(min = 4, max = 16, message = "id는 4자 이상, 16자 이하로 입력해주세요.")
     private String userId;
+
+    @NotBlank(message = "password는 필수 입력 항목입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
+
+    @NotBlank(message = "checkedPassword는 필수 입력 항목입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String checkedPassword;
+
+    @NotBlank(message = "username은 필수 입력 항목입니다.")
     private String username;
 
     public static User toEntity(SignUpForm form) {
